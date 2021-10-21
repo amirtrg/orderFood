69876080d0ea04da477c99cebd3cf7cd71865bbb
reza <amir.torangan@gmail.com>
2021-10-21 20:51:03 +0330

    Card components added to Ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Button from './Components/Ui/Button/Button';
 import "./index.scss"
+import Card from './Components/Ui/Card/Card';
 function App() {
   return (
     <div>
-    <Button>تست</Button>
-    
-    <Button variant="secondary">تست</Button>
+        
     </div>
   );
 }
